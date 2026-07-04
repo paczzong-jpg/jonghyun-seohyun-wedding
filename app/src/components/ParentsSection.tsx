da81faafcc311a7ec091902e0ef7e5e14acba4c4
MISO Coder <coder@miso.gs>
2026-07-04 11:59:16 +0000

    부모님 섹션 이미지를 단일 이미지로 교체 및 배경색 수정

--- app/src/components/ParentsSection.tsx
+++ app/src/components/ParentsSection.tsx
@@ -2,15 +2,10 @@ import { weddingData } from "@/data/weddingData";
 
 export function ParentsSection() {
   return (
-    <section className="w-full bg-transparent text-center p-0 m-0">
+    <section className="w-full bg-white text-center p-0 m-0">
       <img
         className="w-full block"
-        src={weddingData.images.groomFamily}
-        alt=""
-      />
-      <img
-        className="w-full block"
-        src={weddingData.images.brideFamily}
+        src="/images/parents_film.png"
         alt=""
       />
     </section>
